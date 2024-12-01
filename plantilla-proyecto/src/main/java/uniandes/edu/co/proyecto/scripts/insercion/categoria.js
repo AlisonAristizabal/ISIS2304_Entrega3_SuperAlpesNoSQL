43ccf538454d56e9f6c6d14b5d312b50149355da
Nicolas Salazar <n.salazars@uniandes.edu.co>
2024-12-01 01:23:02 -0500

    cambios

--- plantilla-proyecto/src/main/java/uniandes/edu/co/proyecto/scripts/insercion/categoria.js
+++
@@ -1,8 +0,0 @@
-db.categoria.insertOne({
-  id: 1,
-  codigo: 100,
-  nombre: "Electrodomésticos",
-  descripcion: "Categoría de productos electrónicos para el hogar",
-  caracteristica_almacenamiento:
-    "Almacenar en lugares secos y a temperatura ambiente",
-});
